937deeb927f496427161b4a243b4f9958563e76e
s.aran <s-aran@users.noreply.github.com>
2024-12-15 22:47:02 +0900

    add tab

---
+++ ui/src/components/BodyTab.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { JSONEditor } from "svelte-jsoneditor";
+
+  export let getContent: CallableFunction;
+  export let updateCallback: CallableFunction;
+</script>
+
+<div>
+  <JSONEditor content={getContent()} onChange={updateCallback} />
+</div>
