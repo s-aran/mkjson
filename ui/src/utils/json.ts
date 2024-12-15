97266299873b121f5f5743ac7df6104367ca980f
s.aran <s-aran@users.noreply.github.com>
2024-12-16 01:17:27 +0900

    bug fix

--- ui/src/utils/json.ts
+++ ui/src/utils/json.ts
@@ -1,7 +1,7 @@
 import type { Content } from "svelte-jsoneditor";
 
 export const getText = (content: Content) => {
-	if ("text" in Object.keys(content)) {
+	if ("text" in Object.keys(content) && content.text) {
 		return content.text;
 	} else {
 		return JSON.stringify(content.json);
@@ -9,7 +9,7 @@ export const getText = (content: Content) => {
 };
 
 export const getJson = (content: Content) => {
-	if ("json" in Object.keys(content)) {
+	if ("json" in Object.keys(content) && content.json) {
 		return content.json;
 	} else {
 		return JSON.parse(content.text);
